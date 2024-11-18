97ed9e50aeb7d242d4c93e85647f7225223c5863
gabbayron <gabbayron2307@gmail.com>
2024-11-18 16:42:44 +0200

    prettier

--- src/randomFact.ts
+++ src/randomFact.ts
@@ -1,6 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 
 export async function fetchRandomFact() {
-    const response = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random");
-    return response.data.text;
-  }
+  const response = await axios.get("https://uselessfacts.jsph.pl/api/v2/facts/random");
+  return response.data.text;
+}
