7a33c970ef9452ce354d57b9a39ee51c44165c7c
gabbayron <gabbayron2307@gmail.com>
2024-11-18 16:43:39 +0200

    removed unused file

--- src/raidSum.ts
+++ src/raidSum.ts
@@ -12,7 +12,7 @@
 //     if (!fights || fights.length === 0) {
 //       return { raidSummary: "No fights found for this log." };
 //     }
-  
+
 //     let totalDamage = 0;
 //     let totalDamageTaken = 0;
 //     let totalDeaths = 0;
@@ -21,11 +21,11 @@
 //     let playerDamageTaken: { [key: string]: number } = {};
 //     let playerDeaths: { [key: string]: number } = {};
 //     const playerMap: any = await fetchPlayerInfo(logId); // Fetches player names and classes
-  
+
 //     // Iterate through each fight and accumulate data
 //     for (const fight of fights) {
 //       const { startTime, endTime, kill, id: fightId } = fight;
-  
+
 //       // Fetch Damage Data
 //       const {
 //         data: { entries: dmgDone },
@@ -38,7 +38,7 @@
 //           totalDamage += entry.total;
 //         });
 //       }
-  
+
 //       // Fetch Damage Taken Data with spell name filtering
 //       const {
 //         data: { entries: dmgTaken },
@@ -51,7 +51,7 @@
 //           totalDamageTaken += entry.total;
 //         });
 //       }
-  
+
 //       // Fetch Death and Wipe Data
 //       const deathAndWipeData = await fetchDeathsAndWipes(
 //         logId,
@@ -67,18 +67,18 @@
 //           }
 //         });
 //       }
-  
+
 //       if (kill === false) {
 //         totalWipes += 1;
 //       }
 //     }
-  
+
 //     const raidDuration =
 //       (fights[fights.length - 1].endTime - fights[0].startTime) / 1000;
-  
+
 //     const sortedDamageTaken = sortByValueDescending(playerDamageTaken);
 //     const sortedDamageDealers = sortByValueDescending(playerDamage);
-  
+
 //     // Group players by class using playerMap
 //     const groupedByClass: { [classType: string]: string[] } = {};
 //     sortedDamageTaken.forEach((player) => {
@@ -88,7 +88,7 @@
 //       }
 //       groupedByClass[playerClass].push(playerName);
 //     });
-  
+
 //     // Format the roster with each class in a separate row, including class name and icon
 //     const raidRoster = Object.entries(groupedByClass)
 //       .map(
@@ -96,7 +96,7 @@
 //           `${classIcons[playerClass]} ${playerClass}: ${players.join(", ")}`,
 //       )
 //       .join("\n");
-  
+
 //     const dmgDealersSummary = sortedDamageDealers
 //       .slice(0, 10)
 //       .map((player) => {
@@ -104,7 +104,7 @@
 //         return `${classIcons[playerClass] || ""} ${playerName}: ${numberWithCommas(playerDamage[player])}`;
 //       })
 //       .join("\n");
-  
+
 //     const dmgTakenSummary = sortedDamageTaken
 //       .slice(0, 10)
 //       .map((player) => {
@@ -112,7 +112,7 @@
 //         return `${classIcons[playerClass] || ""} ${playerName}: ${numberWithCommas(playerDamageTaken[player])}`;
 //       })
 //       .join("\n");
-  
+
 //     const sortedDeaths = sortByValueDescending(playerDeaths);
 //     const deathsSummary = sortedDeaths
 //       .slice(0, 5)
@@ -121,13 +121,13 @@
 //         return `${playerName}: ${playerDeaths[player]}`;
 //       })
 //       .join("\n");
-  
+
 //     const formattedDate = new Date(raidStartTime).toLocaleDateString("en-US", {
 //       year: "numeric",
 //       month: "short",
 //       day: "numeric",
 //     });
-  
+
 //     const fightsList = fights
 //       .filter((fight) => fight.kill)
 //       .map(
@@ -135,11 +135,10 @@
 //           `**${fight.name}** https://classic.warcraftlogs.com/reports/${logId}#fight=${fight.id}&type=damage-done`,
 //       )
 //       .join("\n");
-  
+
 //     const raidSummary = `**${title} - ${formattedDate} - https://classic.warcraftlogs.com/reports/${logId}\n**\n**Roster** \n${raidRoster}\n\n**Raid Duration**: ${formatDuration(raidDuration)}\n**Total Damage Done**: ${numberWithCommas(totalDamage)}\n**Total Avoidable Damage Taken**: ${numberWithCommas(totalDamageTaken)}\n**Total Deaths**: ${totalDeaths}\n**Total Wipes**: ${totalWipes}\n
 //     \n**Top 10 DPS**:\n${dmgDealersSummary}
 //     \n**Top 10 Avoidable Damage Taken**:\n${dmgTakenSummary}
 //     \n**Deaths by Player (top 5)**:\n${deathsSummary}\n\n**Damage Taken Log breakdown** ${generateWarcraftLogsUrl(logId, dmgTakenFilterExpression)}\n\n`;
 //     return { raidSummary, fightsList };
 //   }
-  
